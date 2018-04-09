b314cc2fccec91bff42d3e12cd837eac69132238
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-09 21:52:30 +0000

    add page template w/ header

---
+++ page-header.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * The template for displaying all pages
+ * Template Name: Page W/ Header
+ *
+ * @since 1.0
+ */
+get_header(); 
+?>
+<div class="w-row">
+  <div class="w-col w-col-10">
+    <h1 class="jer_home_header"><?php wp_title(''); ?></h1>
+  </div>
+  <div class="w-col w-col-2"><img src="/wp-content/uploads/2018/04/jer3.jpg" width="139" class="jer_avatar"></div>
+</div>
