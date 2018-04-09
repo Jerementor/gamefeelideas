4ed08ea917cd7db94b6a0dfaec8c1348a118d1ce
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-09 21:48:04 +0000

    update index

--- index.php
+++ index.php
@@ -9,6 +9,12 @@ get_header();
 ?>
 
 <!-- Index.php -->
+<div class="w-row">
+  <div class="w-col w-col-10">
+    <h1 class="jer_home_header"><?php wp_title(''); ?></h1>
+  </div>
+  <div class="w-col w-col-2"><img src="/wp-content/uploads/2018/04/jer3.jpg" width="139" class="jer_avatar"></div>
+</div>
 <ul>
 <?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : the_post(); ?>
