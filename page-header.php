376316ebfb9c2f8aa39174f655c569783363c08c
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-10 18:58:54 +0000

    undo

--- page-header.php
+++ page-header.php
@@ -11,7 +11,7 @@ get_header();
   <div class="w-col w-col-10">
     <h1 class="jer_home_header"><?php wp_title(''); ?></h1>
   </div>
-  <div class="w-col w-col-2"><img src="<?php if(has_post_thumbnail) the_post_thumbnail(); ?> width="139" class="jer_avatar"></div>
+  <div class="w-col w-col-2"><img src="/wp-content/uploads/2018/04/jer3.jpg" width="139" class="jer_avatar"></div>
 </div>
 <?php 
 if ( have_posts() ) {
