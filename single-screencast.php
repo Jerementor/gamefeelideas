c979d89073cf247b4350606d0ffb5395ee0a7df3
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-12 21:14:56 +0000

    add screencast-video

--- single-screencast.php
+++ single-screencast.php
@@ -10,35 +10,33 @@ get_header();
 
 ?>
 <!--Using SINGLE-SCREENCAST-->
-
-
-<div class="section-140-sides">
-<div style="padding-top:56.17021276595745%" class="w-video w-embed">
-  <!--<iframe class="embedly-embed" src="https://cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FEOQ2hMEKrsM%3Ffeature%3Doembed&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DEOQ2hMEKrsM&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FEOQ2hMEKrsM%2Fhqdefault.jpg&key=96f1f04c5f4143bcb0f2e68c87d65feb&type=text%2Fhtml&schema=youtube" scrolling="no" frameborder="0" allowfullscreen=""></iframe></div>-->
+<!--Screencast Video-->
+<div class="w-video w-embed">
     <?php if (get_field('screencast_video')) :?>
         <div class="embed-container">
             <?php the_field('screencast_video'); ?> 
         </div>
-    <?php endif; ?>    
+    <?php endif; ?>          
 </div>
-</div>
-<div class="section-80">
-    <div class="w-container">
-<h1 class="dap_screencast_title"><?php wp_title(''); ?></h1>
+<!--Screencast Title-->
+<h1 class="jer_screencast_header"><?php wp_title(''); ?></h1>
+<!--Screencast Description-->
 <p>
     <?php if (get_field('screencast_description')) :?>
         <div class="embed-container">
             <?php the_field('screencast_description'); ?> 
         </div>
-    <?php endif; ?>      
+    <?php endif; ?>                
+    
 </p>
-
+<!--Screencast Button-->
 <?php if (get_field('screencast_button')) :?>
-    <a href="<?php the_field('screencast_button') ?>" class="dap_button_large w-button">💬 Leave a Comment in the Community</a>
-<?php endif; ?> 
-
-</div>    
-
+    <a href="<?php the_field('screencast_button') ?>" class="jer_button w-button">💬 Leave a Comment in the Community</a>
+<?php endif; ?>             
+        
 </div>
 
+
 <?php get_footer(); ?>
+
+
