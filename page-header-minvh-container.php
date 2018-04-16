91cbabf02c74b7fd3766a0ebee85c94db16d1b92
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-16 21:40:58 +0000

    thank you pages

---
+++ page-header-minvh-container.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * The template for displaying all pages
+ * Template Name: Page W/ Header & (MINVH) + Container
+ *
+ * @since 1.0
+ */
+get_header(); 
+?>
+<div class="section-60 section-blue">
+  <div class="w-container">
+    <h1 class="jer_post_header_inverse"><?php wp_title(''); ?></h1>
+  </div>
+</div>
+<div class="section-60 section-minvh">
+    <div class="w-container">
+        <div class="jer_640">
+<?php 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); 
+
+		the_content();
+	
+	} // end while
+} // end if
+?>
+</div>
+</div>
+    </div>
+<?php get_footer(); ?>
