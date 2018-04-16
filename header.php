0145a54df3269d48529024cd76e907eda0a71edc
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-16 22:00:23 +0000

    new fonts

--- header.php
+++ header.php
@@ -11,8 +11,9 @@
     && get_option( 'thread_comments' ) ) 
     wp_enqueue_script( 'comment-reply' ); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic","Playfair Display:regular,700,700italic,900,900italic","News Cycle:regular,700","Rubik:300,300italic,regular,italic,500,500italic,700,700italic,900,900italic","Source Code Pro:200,300,regular,500,600,700,900"]  }});</script>
 
-    <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic"]  }});</script>
+    <!--<script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic"]  }});</script>-->
     <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
 
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
