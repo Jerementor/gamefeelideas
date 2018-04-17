7e589ec9b29f7ee70786f75b5c5d6f17346280c2
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-17 01:40:51 +0000

    single.php edited

--- single.php
+++ single.php
@@ -20,9 +20,9 @@ get_header();
 <div class="section-60">
 <div class="w-container">
 <div class="jer_640"> 
-<div class="jer_meta_categories">
-<?php the_category(' ', 'multiple')?>
-</div>
+<!--<div class="jer_meta_categories">-->
+<!--<?php the_category(' ', 'multiple')?>-->
+<!--</div>-->
 <div class="jer_blog">
     	<?php 
 		if ( have_posts() ) {
@@ -36,22 +36,22 @@ get_header();
 	?>
 </div>
 <!--end jer_blog-->
-<div class="jer_single_footer">
-   <div class="w-form">
-          <form id="wf-form-gfi_join_newsletter" name="wf-form-gfi_join_newsletter" data-name="gfi_join_newsletter" method="post" action="https://www.getdrip.com/forms/209451262/submissions" data-drip-embedded-form="209451262">
-            <div class="w-row">
-              <div class="w-col w-col-6"><input type="text" class="jer_input w-input" maxlength="256" name="fields[email]" data-name="fields[email]" placeholder="Enter your email...." id="fields[email]" required=""></div>
-              <div class="w-col w-col-6"><input type="submit" value="Get Weekly Articles" data-wait="Please wait..." class="submit-button w-button"></div>
-            </div>
-          </form>
-          <div class="success-message w-form-done">
-            <div>Thank you! Your submission has been received!</div>
-          </div>
-          <div class="error-message w-form-fail">
-            <div>Oops! Something went wrong while submitting the form.</div>
-          </div>
-        </div>    
-</div>
+<!--<div class="jer_single_footer">-->
+<!--   <div class="w-form">-->
+<!--          <form id="wf-form-gfi_join_newsletter" name="wf-form-gfi_join_newsletter" data-name="gfi_join_newsletter" method="post" action="https://www.getdrip.com/forms/209451262/submissions" data-drip-embedded-form="209451262">-->
+<!--            <div class="w-row">-->
+<!--              <div class="w-col w-col-6"><input type="text" class="jer_input w-input" maxlength="256" name="fields[email]" data-name="fields[email]" placeholder="Enter your email...." id="fields[email]" required=""></div>-->
+<!--              <div class="w-col w-col-6"><input type="submit" value="Get Weekly Articles" data-wait="Please wait..." class="submit-button w-button"></div>-->
+<!--            </div>-->
+<!--          </form>-->
+<!--          <div class="success-message w-form-done">-->
+<!--            <div>Thank you! Your submission has been received!</div>-->
+<!--          </div>-->
+<!--          <div class="error-message w-form-fail">-->
+<!--            <div>Oops! Something went wrong while submitting the form.</div>-->
+<!--          </div>-->
+<!--        </div>    -->
+<!--</div>-->
 <!--end jer footer-->
 </div>
 </div>
