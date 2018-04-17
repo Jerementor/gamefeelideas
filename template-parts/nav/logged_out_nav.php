4e93aa5919e2daf4604d4d6d191024a2d48ed30e
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-17 02:17:34 +0000

    wider nav

--- template-parts/nav/logged_out_nav.php
+++ template-parts/nav/logged_out_nav.php
@@ -1,13 +1,12 @@
 <div data-collapse="medium" data-animation="default" data-duration="400" class="dap_navbar w-nav">
- <div class="w-container">
-   <a href="/" class="w-nav-brand">
-     <h3 class="game_logo">dev <em>and</em> play</h3>
-   </a>
-   <nav role="navigation" class="nav-menu w-nav-menu">
+  <a href="index.html" class="w-nav-brand">
+    <h3 class="game_logo">dev <em class="italic-text">and</em> play</h3>
+  </a>
+  <nav role="navigation" class="nav-menu w-nav-menu">
    <?php get_template_part( 'template-parts/nav/nav_out'); ?>    
+
    </nav>
-   <div class="menu-button w-nav-button">
-     <div class="w-icon-nav-menu"></div>
-   </div>
- </div>
+  <div class="menu-button w-nav-button">
+    <div class="w-icon-nav-menu"></div>
+  </div>
 </div>
